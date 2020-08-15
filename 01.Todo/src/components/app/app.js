9ea6e5b52295c67ba28750465500e1c51017e579
Serhii Kostenko <sergey@coderiver.com.ua>
2020-08-15 18:43:23 +0300

    removed comments

--- 01.Todo/src/components/app/app.js
+++ 01.Todo/src/components/app/app.js
@@ -48,29 +48,3 @@ export default class App extends Component{
     );
   }
 }
-
-// const App = () => {
-
-//   // const todoData = [
-//   //   { label: 'Drink Coffee', important: false, id: 1 },
-//   //   { label: 'Make Awesome App', important: true, id: 2 },
-//   //   { label: 'Have a lunch', important: false, id: 3 }
-//   // ];
-
-//   return (
-//     <div className="todo-app">
-//       <AppHeader toDo={1} done={3} />
-//       <div className="top-panel d-flex">
-//         <SearchPanel />
-//         <ItemStatusFilter />
-//       </div>
-
-//       <TodoList 
-//         todos={todoData}
-//         onDeleted={(id)=>{console.log(`del ${id}`)}}
-//         />
-//     </div>
-//   );
-// };
-
-// export default App;
