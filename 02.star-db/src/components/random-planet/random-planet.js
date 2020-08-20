ed05a606c8cf87e8654e32fb3bf41a64389a869e
Serhii Kostenko <sergey@coderiver.com.ua>
2020-08-20 10:14:55 +0300

    upd swapi-service

--- 02.star-db/src/components/random-planet/random-planet.js
+++ 02.star-db/src/components/random-planet/random-planet.js
@@ -8,11 +8,7 @@ export default class RandomPlanet extends Component {
   swapiService = new swapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotatioPeriod: null,
-    diameter: null
+    planet: {}
   }
 
   constructor(){
@@ -20,22 +16,17 @@ export default class RandomPlanet extends Component {
     this.updatePlanet()
   }
 
+  onPlanetLoaded = (planet) => {
+    this.setState({planet})
+  }
   updatePlanet() {
     const id = Math.floor(Math.random()* 25) + 2;
     this.swapiService
       .getPlanet(id)
-      .then((planet) => {
-        this.setState({
-          id,
-          name: planet.name,
-          population: planet.population,
-          rotatioPeriod: planet.rotation_period,
-          diameter: planet.diameter
-        })
-      })
+      .then(this.onPlanetLoaded)
   }
   render() {
-    const { id, name, population, rotatioPeriod, diameter } = this.state;
+    const { id, name, population, rotatioPeriod, diameter } = this.state.planet;
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
