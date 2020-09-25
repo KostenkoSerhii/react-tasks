356ffd825259215464e8f0301fd0cd56a4cea6ee
Serhii Kostenko <sergey@coderiver.com.ua>
2020-09-25 10:15:45 +0300

    added basic components

---
+++ 06.re-store/src/components/error-boundry/error-boundry.js
@@ -0,0 +1,19 @@
+import ErrorIndicator from 'components/error-indicator';
+import React, {Component} from 'react';
+
+export default class ErrorBoundry extends Component{
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true })
+  };
+
+  render(){
+    if(this.state.hasError){
+      return <ErrorIndicator/>
+    }
+    return this.props.children
+  }
+}
