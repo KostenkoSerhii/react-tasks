fcc3c825b0570f58787f460ac0719b3e030ce1a5
Serhii Kostenko <sergey@coderiver.com.ua>
2020-09-29 14:52:01 +0300

    added thunk

--- 06.re-store/src/store.js
+++ 06.re-store/src/store.js
@@ -1,7 +1,16 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
+const myActionCreator = (timeout) =>(dispatch) => {
+  setTimeout(() => dispatch({
+    type: 'TIME_OUT'
+  }), timeout) 
+}
+
+store.dispatch(myActionCreator(3000))
 export default store;
